213f1d2a8b281874085433fe78e79019a4a3f875
Taylor Malloy <mail@taylormalloy.com>
2021-04-25 19:12:34 +0200

    Move numbers to left hand
    Will let me use mouse with right hand while left hand is typing numbers.

--- keyboards/crkbd/keymaps/tmalloy/keymap.c
+++ keyboards/crkbd/keymaps/tmalloy/keymap.c
@@ -29,24 +29,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_GRV,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_MINS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-   LT(2, KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, LT(2, KC_QUOT),
+LT(2, KC_TAB),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, LT(2, KC_QUOT),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                              TT(1) , CMD_T(KC_ESC), CTL_T(KC_BSPC),   ALT_T(KC_SPC), KC_ENT, OSL(3)
+                             TG(1) , CMD_T(KC_ESC),  CTL_T(KC_BSPC),   ALT_T(KC_SPC), LT(1, KC_ENT), OSL(3)
                                       //`--------------------------'  `--------------------------'
 
   ),
 
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_PLUS,    KC_7,    KC_8,    KC_9,  KC_EQL, KC_BSPC,
+       KC_DOT, KC_PLUS,    KC_7,    KC_8,    KC_9,  KC_EQL,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_MINS,    KC_4,    KC_5,    KC_6, KC_ASTR,  KC_ENT,
+       KC_TAB, KC_MINS,    KC_4,    KC_5,    KC_6, KC_ASTR,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         KC_0,    KC_1,    KC_2,    KC_3, KC_SLSH, KC_DOT,
+      KC_LSFT,    KC_0,    KC_1,    KC_2,    KC_3, KC_SLSH,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            TG(1), XXXXXXX, XXXXXXX,     KC_SPC, XXXXXXX, XXXXXXX
+                             TG(1) , CMD_T(KC_ESC),  CTL_T(KC_BSPC),   ALT_T(KC_SPC), KC_ENT, OSL(3)
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -178,3 +178,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 #endif // OLED_DRIVER_ENABLE
+
